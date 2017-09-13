b941cab382af249a8d73a3df679823e7cae1a786
Kunle Adeyinka <aadeyink@gmail.com>
2017-09-13 09:16:00 +0000

    added form functionality for authors, books, genres, instances

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -32,12 +32,46 @@ exports.genre_detail = function(req, res, next) {
 
 // Display Genre create form on GET
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', {title: 'Create A Genre'});
 };
 
 // Handle Genre create on POST
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
+exports.genre_create_post = function(req, res, next) {
+    
+    //check that the name field is not empty
+    req.checkBody('name', 'Genre name required').notEmpty();
+    
+    //trim and excape the name field
+    req.sanitize('name').escape();
+    req.sanitize('name').trim();
+    
+    //run the validators
+    var errors = req.validationErrors();
+    
+    //create a genre object with escaped and trimmed data
+    var genre = new Genre(
+        {name:req.body.name}
+    );
+    
+    if(errors){
+        res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors});
+        return;
+    }else{
+        Genre.findOne({'name': req.body.name}).exec(function(err, found_genre){
+           console.log('found_genre: ' + found_genre);
+           
+           if(err){return next(err)}
+           
+           if(found_genre){
+               res.redirect(found_genre.url);
+           }else{
+               genre.save(function(err){
+                   if(err){return next(err)}
+                   res.redirect(genre.url);
+               });
+           }
+        });
+    }
 };
 
 // Display Genre delete form on GET
