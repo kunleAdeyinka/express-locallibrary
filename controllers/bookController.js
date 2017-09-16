7f470790ead67d60c1fa6943c5e52e9b1a213cff
Kunle Adeyinka <aadeyink@gmail.com>
2017-09-16 10:17:32 +0000

    added delete functionality to all entities

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -133,13 +133,48 @@ exports.book_create_post = function(req, res, next) {
 };
 
 // Display book delete form on GET
-exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+exports.book_delete_get = function(req, res, next) {
+    
+    async.parallel({
+        book: function(callback){
+            Book.findById(req.params.id).exec(callback);
+        },
+        book_instances: function(callback){
+            BookInstance.find({ 'book': req.params.id }).exec(callback);
+        },
+    }, function(err, results) {
+        if(err){return next(err);}
+        
+        res.render('book_delete', {title: 'Delete Book', book: results.book, book_instances: results.book_instances });
+    });
 };
 
 // Handle book delete on POST
-exports.book_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete POST');
+exports.book_delete_post = function(req, res, next) {
+    
+    req.checkBody('bookid', 'Book id must exist').notEmpty();
+    
+    async.parallel({
+        book: function(callback){
+            Book.findById(req.body.bookid).exec(callback);
+        },
+        book_authors: function(callback){
+            Author.find({'book': req.body.bookid}, 'first_name family_name').exec(callback);
+        }
+    }, function(err, results) {
+        if(err){return next(err);}
+        
+        if(results.book_authors.length > 0){
+            res.render('book_delete', { title: 'Delete Book', book: results.book, book_authors: results.book_authors  } );
+            return;
+        }else{
+            Book.findByIdAndRemove(req.body.bookid, function deleteBook(err) {
+                if (err) { return next(err); }
+                //Success - got to author list
+                res.redirect('/catalog/books');
+            });
+        }
+    });
 };
 
 // Display book update form on GET
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -71,13 +71,22 @@ exports.bookinstance_create_post = function(req, res, next) {
 };
 
 // Display BookInstance delete form on GET
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function(req, res, next) {
+    
+    BookInstance.findById(req.params.id).populate('book').exec(function (err, bookinstance) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('bookinstance_delete', { title: 'Delete BookInstance', bookinstance: bookinstance } );
+    });
 };
 
 // Handle BookInstance delete on POST
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function(req, res, next) {
+    BookInstance.findByIdAndRemove(req.params.id, function deleteAuthor(err) {
+        if (err) { return next(err); }
+        
+        res.redirect('/catalog/bookinstances');
+    });
 };
 
 // Display BookInstance update form on GET
